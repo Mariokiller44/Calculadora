c13b74a9a24fdd0748028076a9219e8064498f72
Mariokiller44 <84866133+Mariokiller44@users.noreply.github.com>
2021-11-23 13:39:38 +0100

    commit2.0

--- Operadores.java
+++ Operadores.java
@@ -2,7 +2,7 @@ public class Operadores{
 
   public static void main (String[] args){
       
-    System.out.println("Hola grupo");  
+    System.out.println("Hola grupo soy vanesa");  
     
   }
 
