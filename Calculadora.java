f864552210839fefcf8cfee2e40d439de0b39ffb
LosMinerosDeLaProgramacion <94907179+LosMinerosDeLaProgramacion@users.noreply.github.com>
2021-12-14 12:12:47 +0100

    actualizada de errores

--- Calculadora.java
+++ Calculadora.java
@@ -1,4 +1,3 @@
-package BuclesNumeros;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,18 +6,25 @@ public class Calculadora {
 
 	static double numero1 , numero2;//operandos aritmeticos
 	static int operacion, tipoOperacion;//seleccionar operacion y tipo de operacion
-	static boolean salir = false, booleano1, booleano2;//variable bandera y operandos logicos
+	static boolean salir = false, booleano1 = false, booleano2 = false;//variable bandera y operandos logicos
 	static int respuesta;//para repetir operacion
 	static InputStreamReader isr = new InputStreamReader(System.in);
 	static BufferedReader br = new BufferedReader (isr);
 	
-	public static void main(String[] args) throws  NumberFormatException, IOException {
+	public static void main(String[] args) throws  IOException {
 		
 		do {
-			
-		menu();
-		elegirMenu(tipoOperacion);
-		repetirPrograma();
+			try {
+				menu();
+				elegirMenu(tipoOperacion);
+				repetirPrograma();
+			}
+			catch(NumberFormatException nfe) {
+				System.out.println("Eso no es un valor valido.");
+			}
+		
+		
+		
 		
 		}while(salir == false);
 		
@@ -26,20 +32,22 @@ public class Calculadora {
 	
 	//ELEGIR ENTRE MENU ARITMETICO O LOGICO
 	
-	public static void menu() throws IOException {
-		System.out.println("Elige que hacer:");
-		System.out.println("1.Operación Aritmética");
-		System.out.println("2.Operación Lógica");
-		try {
-		tipoOperacion = Integer.parseInt(br.readLine());
-		}catch(NumberFormatException nfe) {
-			System.out.println("Error, el valor introducido no es valido.");
-		}
+	public static void menu() throws IOException, NumberFormatException {
+		do {
+			System.out.println("Elige que hacer:");
+			System.out.println("1.OperaciÃ³n AritmÃ©tica");
+			System.out.println("2.OperaciÃ³n LÃ³gica");
+		
+			tipoOperacion = Integer.parseInt(br.readLine());
+			if (tipoOperacion < 1 || tipoOperacion > 2) {
+				System.out.println("No has elegido una opcion correcta.");
+			}
+		}while(tipoOperacion < 1 || tipoOperacion > 2);
 	}
 	
 	//EJECUCION DEL MENU ARITMETICO O LOGICO
 	
-	public static void elegirMenu(int elegir) throws NumberFormatException, IOException {
+	public static void elegirMenu(int elegir) throws IOException, NumberFormatException {
 		switch (elegir) {
 		case 1: {
 			
@@ -62,77 +70,82 @@ public class Calculadora {
 	
 	//SELECCIONAR OPERACION ARITMETICA
 	
-	public static void elegirOperacionAritmetica() throws  NumberFormatException, IOException {
-		System.out.println("Elige: ");
-		System.out.println("1.Sumar");
-		System.out.println("2.Restar");
-		System.out.println("3.Multiplicar");
-		System.out.println("4.Dividir");
-		System.out.println("5.Modulo");
+	public static void elegirOperacionAritmetica() throws  IOException, NumberFormatException {
+		do {
+			System.out.println("Elige: ");
+			System.out.println("1.Sumar");
+			System.out.println("2.Restar");
+			System.out.println("3.Multiplicar");
+			System.out.println("4.Dividir");
+			
 		
-		try {
-		operacion = Integer.parseInt(br.readLine());
-		}catch(NumberFormatException nfe) {
-			System.out.println("Error, el valor introducido no es valido.");
-		}
+			operacion = Integer.parseInt(br.readLine());
+			if (operacion < 1 || operacion > 4) {
+				System.out.println("No has elegido una opcion correcta.");
+			}
+		}while(operacion < 1 || operacion > 4);
 	}
 	
 	//SELECCIONAR OPERACION LOGICA
 	
-	public static void elegirOperacionLogica() throws  NumberFormatException, IOException {
-		System.out.println("Elige para comparar dos numeros: ");
-		System.out.println("1.AND");
-		System.out.println("2.OR");
-		System.out.println("3.NOT");
-		System.out.println("4.XOR");
-		System.out.println("5.NAND");
-		System.out.println("6.NOR");
-		try {
-		operacion = Integer.parseInt(br.readLine());
-		}catch(NumberFormatException nfe) {
-			System.out.println("Error, el valor introducido no es valido.");
-		}
+	public static void elegirOperacionLogica() throws  IOException, NumberFormatException {
+		do {
+			System.out.println("Elige para comparar dos numeros: ");
+			System.out.println("1.AND");
+			System.out.println("2.OR");
+			System.out.println("3.NOT");
+			System.out.println("4.XOR");
+			System.out.println("5.NAND");
+			System.out.println("6.NOR");
+		
+			operacion = Integer.parseInt(br.readLine());
+			if (operacion < 1 || operacion > 6) {
+				System.out.println("No has elegido una opcion correcta.");
+			}
+		}while(operacion < 1 || operacion > 6);
 		
 	}
 	
 	//OPERANDOS ARITMETICOS
 	
-	public static void operandosAritmeticos() throws  NumberFormatException, IOException {
-		try {
-		System.out.println("Añada el primer numero: ");
+	public static void operandosAritmeticos() throws  IOException {
+		System.out.println("AÃ±ada el primer numero: ");
 		numero1 = Double.parseDouble(br.readLine());
 		
 		do {
-			System.out.println("Añada el segundo numero: ");
+			System.out.println("AÃ±ada el segundo numero: ");
 			numero2 = Double.parseDouble(br.readLine());
 			if(operacion == 4 && numero2 == 0) {
-				System.out.println("No se puede dividir un número entre 0.");
+				System.out.println("No se puede dividir un nÃºmero entre 0.");
 				System.out.println("Pruebe de nuevo.");
 			}
 		}
 		while(operacion == 4 && numero2 == 0);
 		
-		}catch(NumberFormatException nfe) {
-			System.out.println("Error, el valor introducido no es valido.");
-		}
 		
 	}
 	
 	//OPERANDOS LOGICOS
 	
-	public static void operandosLogicos() throws  NumberFormatException, IOException {
-		try {
-		System.out.println("Añada 'true' o 'false': ");
-		booleano1 = Boolean.parseBoolean(br.readLine());
-		//System.out.println(b1); comprobación de valor
-		if (operacion!=3)	{
-			System.out.println("Añada 'true' o 'false': ");
-			booleano2 = Boolean.parseBoolean(br.readLine());
-		}	
+	public static void operandosLogicos() throws  IOException {
+		do {
+			System.out.println("AÃ±ada el 1Âº '1' para 'true' o '0' para 'false': ");
+			numero1 = Double.parseDouble(br.readLine());
+			if(numero1 == 1)
+				booleano1 = true;
+			else
+				booleano1 = false;
+			//System.out.println(b1); comprobaciÃ³n de valor
+			if (operacion!=3)	{
+				System.out.println("AÃ±ada el 2Âº '1' para 'true' o '0' para 'false': ");
+				numero2 = Double.parseDouble(br.readLine());
+				if(numero2 == 1)
+					booleano2 = true;
+				else
+					booleano2 = false;
+			}	
 		
-		}catch(NumberFormatException nfe) {
-			System.out.println("Error, el valor introducido no es valido.");
-		}
+		}while(numero1 != 1 && numero1 != 0 || numero2 != 1 && numero2 != 0);
 	}
 	
 	//ELEGIR OPERACION ARITMETICA
@@ -151,9 +164,7 @@ public class Calculadora {
 		case 4: {
 			System.out.println(hacerDivision(numero1,numero2));	
 		}break;
-		case 5: {
-			System.out.println(hacerModulo(numero1,numero2));			
-		}break;
+		
 		default:
 			System.out.println("No has elegido una oopcion correcta...");
 		}
@@ -191,18 +202,14 @@ public class Calculadora {
 	
 	//REPETIR PROGRAMA
 	
-	public static boolean repetirPrograma() throws NumberFormatException, IOException {
+	public static boolean repetirPrograma() throws IOException, NumberFormatException {
 
 		do {
 			
-			System.out.println("¿Quieres hacer otra operación? ");
+			System.out.println("Â¿Quieres hacer otra operaciÃ³n? ");
 			System.out.println("1.Repetir ");
 			System.out.println("0.Salir ");
-			try {
 			respuesta = Integer.parseInt(br.readLine());
-			}catch(NumberFormatException nfe) {
-				System.out.println("Error, el valor introducido no es valido.");
-			}
 			
 		}while(respuesta !=0 && respuesta != 1);
 		
@@ -217,7 +224,6 @@ public class Calculadora {
 		
 		
 	}
-	
 	
 	//OPERACIONES ARITMETICAS
 	
@@ -234,12 +240,9 @@ public class Calculadora {
 	}
 	
 	private static double hacerDivision(double numero1, double numero2) {
-		return ((double) numero1 / numero2);
+		return (numero1 / numero2);
 	}
 	
-	private static double hacerModulo(double numero1, double numero2) {
-		return (numero1 % numero2);
-	}
 	
 	
 	//OPERACIONES LOGICAS
@@ -267,5 +270,4 @@ public class Calculadora {
 	private static boolean hacerNor(boolean b1, boolean b2) {
 		return (!b1 || !b2);
 	}
-	
 }
